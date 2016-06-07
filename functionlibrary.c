0233f37849b494b8013b6f76f783fbe9120fb383
hariharansathyanarayanan <hariharan.sathyanarayanan@gmail.com>
2016-06-07 14:29:18 +0000

    library files uploaded.

---
+++ functionlibrary.c
@@ -0,0 +1,48 @@
+#include "functionlibrary.h"
+#include <stdio.h>
+
+void insertionsort(int A[],int length) {
+	int j,i,key;
+	for(j=1;j<length;j++) {
+		i = j-1;
+		key = A[j];
+		while(i >= 0 && A[i] > key) {
+			A[i+1] = A[i];
+			i = i-1;
+		}
+		A[i+1] = key;
+	}
+	//sorting finished
+}
+
+void print(int A[],int size) {
+	int i = 0;
+	fprintf(stdout,"A elements:\n");
+	while (i < size) {
+		printf("A[%d] - %d\n",i,A[i]);
+		i++;
+	}
+	//printing finished
+}
+
+void recursive_sort(int A[],int j,int r) {
+	if(j < 0) {
+		printf("\nj less than zero\n");
+		return;
+	}
+	if(r) {
+		printf("\ncompell recursion time for %d\n",j);
+		recursive_sort(A,j-1,r);
+		
+	}
+	if(A[j] > A[j+1]) {
+		printf("\nA[%d] > A[%d] i.e %d > %d\n",j,j+1,A[j],A[j+1]);
+	// swap A[j] with A[j+1]
+		int temp = A[j];
+		A[j] = A[j+1];
+		A[j+1] = temp;
+		printf("\nSwap finished recursion for %d\n",j);
+		print(A,8);
+		recursive_sort(A,j-1,0);
+	}
+}
---
+++ functionlibrary.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONLIBRARY
+#define FUNCTIONLIBRARY
+	void insertionsort(int[],int);
+	void print(int[],int);
+	void recursive_sort(int[],int,int);
+#endif
